430375af85d89f052dbafb926ed41a4f99ec1a14
Riley Timlin <122389474+timlinriley@users.noreply.github.com>
2023-02-14 04:18:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const minusOne = (num) =>{
+  return num - 1;
+}
+console.log(minusOne(4))
+
+function makeSentence(x,y,z){
+  let string= `${x}  ${y}  ${z}`
+  console.log(string)
+  
+} makeSentence('i','want','chimichangas')
+
+let arr = [1,2,3,4,5]
+function getLastElement(arr){
+
+  console.log(arr[arr.length-1])
+  
+}
+getLastElement(arr)
